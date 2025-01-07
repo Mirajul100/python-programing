c4e1a800c0a6426a2aba3349fe2cd3a228e8495e
Anik <mirajulislamanik100@gmail.com>
2025-01-07 17:23:06 +0600

    master

---
+++ input.py
@@ -0,0 +1,17 @@
+a = int (input ("Enter the 1 number : "))
+
+b = int (input("Enter the 2 number : "))
+
+name = input ("Enter your name : ")
+
+age = int (input ("Enter your age : "))
+
+
+print ("1 number is : " , a)
+print ("1 number is : " , b)
+
+print ("Sum of the number is : " , a + b)
+
+print ("The name is : " , name)
+print ("The age is : " , age)
+print ("name")
