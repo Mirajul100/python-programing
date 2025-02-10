6be3aa5735042b557735093d041e745934bfa51e
Anik <mirajulislamanik100@gmail.com>
2025-02-11 00:09:33 +0600

    match

--- match.py
+++ match.py
@@ -4,15 +4,18 @@ while True :
     user_input = input ("Enter add , show , exit : ")
     user_input = user_input.strip()
 
-    match user_input:
+    match user_input.lower():
         case "add" :
             value = input("Enter the value : ")
             values.append(value.capitalize())
         case "show":
             for trop in values:
-                print (trop)
+                # trop = trop.title()
+                print (trop.title())
         case "exit":
             break
+        case unknown:
+            print ("You print wrong value")
 
 print("Program is ended")
 
