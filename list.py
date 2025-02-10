80e023d777c170752bb3b7cd6d3f39e4611257e7
Anik <mirajulislamanik100@gmail.com>
2025-02-10 17:49:25 +0600

    while loop

---
+++ list.py
@@ -0,0 +1,15 @@
+first_name = input ("Enter the first name : ")
+middle_name = input ("Enter the middle name : ")
+last_name = input ("Enter the last name : ")
+
+person = [first_name , middle_name , last_name , "Bangladesh"]
+
+print("person : ", person)
+
+words = ["good morning" , "dog" , "cat"]
+
+print (words)
+
+print(type(words))
+print(type(first_name + middle_name + last_name))
+print (type(person))
---
+++ whileLoop.py
@@ -0,0 +1,9 @@
+students = []
+
+while True:
+    student = input ("Enter the student name :")
+    students.append(student.capitalize())
+    print (students)
+
+
+    
