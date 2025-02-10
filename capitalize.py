bf2daeb06cf418039eac90d15cb1e6af99964bd2
Anik <mirajulislamanik100@gmail.com>
2025-02-10 18:34:46 +0600

    capitalize

---
+++ capitalize.py
@@ -0,0 +1,11 @@
+string = "what is your naem : "
+
+sentence = "Enter the sentence : "
+
+while True:
+    name = input (string)
+    print (name.capitalize())
+
+    word = input (sentence)
+    print (word.title())
+
