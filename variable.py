9281e77583ca4e606e4003266dce36e40967553b
Anik <mirajulislamanik100@gmail.com>
2025-02-10 20:34:13 +0600

    variable

--- variable.py
+++ variable.py
@@ -1,7 +1,9 @@
 name = input("Enter your name : ")
 city = input("Enter your city name : ")
 country = input("Enter your country name : ")
+age = input ("Enter your age : ")
 
 print ("name :", name)
 print ("city name :", city)
 print ("country name : "+country)
+print ("age is : ",age)
