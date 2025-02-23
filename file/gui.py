b35a3039989999a4d05fe8e57911d23c4a429e33
Anik <mirajulislamanik100@gmail.com>
2025-02-23 23:11:43 +0600

    FILE

--- file/gui.py
+++ file/gui.py
@@ -1,9 +1,27 @@
 import FreeSimpleGUI as frame
+import function
 
-label = frame.Text("Enter the Employee Name : ")
-inputBox = frame.InputText(tooltip="Enter the Employee Name")
+label = frame.Text("Enter the Employee Name : " , tooltip="Enter Employee")
+inputBox = frame.InputText(tooltip="Enter the Employee Name" , key="employee")
 addButton = frame.Button("Add")
 
-windows = frame.Window("Employee management" , layout=[[label] , [inputBox , addButton]])
-windows.read()
+windows = frame.Window("Employee management" ,
+                        layout=[[label] , 
+                        [inputBox , addButton]] , 
+                        font=("Roboto",18))
+
+while True:
+    userInput , value = windows.read()
+    print (userInput)
+    print (value)
+
+    match userInput:
+        case "Add":
+            employee = function.readFile()
+            newEmployee = value["employee"]+"\n"
+            employee.append(newEmployee)
+            function.writeFile(employee)
+        case frame.WIN_CLOSED:
+            break
+
 windows.close()
