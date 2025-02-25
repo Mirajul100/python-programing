777c42f0f6830f55adbe0efa41bd0d28e83e648c
Anik <mirajulislamanik100@gmail.com>
2025-02-26 01:42:47 +0600

    FILE

--- file/gui.py
+++ file/gui.py
@@ -4,11 +4,16 @@ import function
 label = frame.Text("Enter the Employee Name : " , tooltip="Enter Employee")
 inputBox = frame.InputText(tooltip="Enter the Employee Name" , key="employee")
 addButton = frame.Button("Add")
+ListBox = frame.Listbox(values=function.readFile() ,
+                      key="edit" , enable_events= True ,
+                      size=(50 , 10))
+
+editButton = frame.Button("Edit")
 
 windows = frame.Window("Employee management" ,
                         layout=[[label] , 
-                        [inputBox , addButton]] , 
-                        font=("Roboto",18))
+                        [inputBox , addButton] , [ListBox ,editButton]] , 
+                        font=("Helvetica",18))
 
 while True:
     userInput , value = windows.read()
@@ -21,6 +26,21 @@ while True:
             newEmployee = value["employee"]+"\n"
             employee.append(newEmployee)
             function.writeFile(employee)
+            windows["edit"].update(values=employee)
+
+        case "Edit":
+            select = value["edit"][0]
+            newEdit = value["employee"]
+
+            employees = function.readFile()
+            index = employees.index(select)
+            employees[index] = newEdit
+            function.writeFile(employees)
+            windows["edit"].update(values=employees)
+
+        case "edit":
+            windows["employee"].update(value=value["edit"][0])
+
         case frame.WIN_CLOSED:
             break
 
