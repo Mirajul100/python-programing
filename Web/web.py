43411169c37bc287cac25580214eb54d36bba5c0
Anik <mirajulislamanik100@gmail.com>
2025-03-19 01:24:16 +0600

    web

--- Web/web.py
+++ Web/web.py
@@ -4,5 +4,9 @@ import function
 st.title("Student management")
 
 st.subheader("This is my student management system")
-st.text_input (label="Enter name" , placeholder="Enter student name..")
 st.write ("Select the student name")
+st.text_input (label="Enter name" , placeholder="Enter student name..")
+
+for name in function.readFile():
+    st.checkbox(name.title())
+
