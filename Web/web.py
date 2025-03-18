dd74fcd9e2e5d1c56f2d6acc1cc6779b73afe746
Anik <mirajulislamanik100@gmail.com>
2025-03-19 01:54:52 +0600

    Wed

--- Web/web.py
+++ Web/web.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import function
 
+studentName = function.readFile()
+
+def add_student():
+    student = st.session_state["newStudentName"] + "\n"
+    studentName.append(student)
+    function.writeFile(studentName)
+
 st.title("Student management")
 
 st.subheader("This is my student management system")
-st.text_input (label="Enter name" , placeholder="Enter student name..")
+st.text_input (label="Enter name" , placeholder="Enter student name.." , 
+               on_change=add_student , key="newStudentName")
 st.write ("Select the student name")
 
-for name in function.readFile():
+for name in studentName:
     st.checkbox(name.title())
 
