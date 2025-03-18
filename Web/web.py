4a9f7419683931748856197a0a66048d2b57ebbb
Anik <mirajulislamanik100@gmail.com>
2025-03-19 01:25:25 +0600

    Web

--- Web/web.py
+++ Web/web.py
@@ -4,8 +4,8 @@ import function
 st.title("Student management")
 
 st.subheader("This is my student management system")
-st.write ("Select the student name")
 st.text_input (label="Enter name" , placeholder="Enter student name..")
+st.write ("Select the student name")
 
 for name in function.readFile():
     st.checkbox(name.title())
