2f5c3f7728eb99c567e5cffc640eed9d47bff1b2
Anik <mirajulislamanik100@gmail.com>
2025-03-24 22:27:20 +0600

    My Protfolio Website

--- protfolio/pages/My_CV.py
+++ protfolio/pages/My_CV.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("This is my Curriculum vitae")
+content = "This is my Curriculum vitae"
+st.title(content.title())
 st.info("[Resume Link](https://drive.google.com/file/d/1OWRmbqiaS4EZsXh57Mf-Z88n8RV0oidu/view?usp=sharing)")
