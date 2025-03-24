a0dc7d0eced6b6c920bdcc1df8fe36e331127fa4
Anik <mirajulislamanik100@gmail.com>
2025-03-24 16:09:01 +0600

    My protfolio Website

--- protfolio/sendEmail.py
+++ protfolio/sendEmail.py
@@ -1,11 +1,12 @@
 import smtplib , ssl
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     userEmail = "mirajulislamanik100@gmail.com"
-    password = "lpwx vfpk jiyh lpzq"
+    password = os.getenv("PASSWORD")
 
     receiver = "mirajulislamanik100@gmail.com"
     context = ssl.create_default_context()
