7c5913af7dafa68380c3ed8141dec8ba8a71343a
Anik <mirajulislamanik100@gmail.com>
2025-03-27 16:05:44 +0600

    Student Todo List

--- Web/web.py
+++ Web/web.py
@@ -3,13 +3,15 @@ import function
 
 studentName = function.readFile()
 
+st.set_page_config(layout="centered")
+
 def add_student():
     new_student = st.session_state["newStudent"] + "\n"
     studentName.append(new_student)
     function.writeFile(studentName)
 
-st.title("Student management system")
-st.text_input(label="Select the student name", 
+st.title("Student Name Todo List")
+st.text_input(label="Add the student name", 
               placeholder="Enter student name..", 
               on_change=add_student , key= "newStudent")
 
