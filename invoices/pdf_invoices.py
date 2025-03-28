3c45213c58946810e6a761d8ca68a3839fa9f400
Anik <mirajulislamanik100@gmail.com>
2025-03-29 00:09:46 +0600

    Invoices

--- invoices/pdf_invoices.py
+++ invoices/pdf_invoices.py
@@ -8,13 +8,12 @@ file_path = glob.glob("invoices/xl/*.xlsx")
 for file in file_path:
     df = pd.read_excel(file , sheet_name="Sheet 1")
     pdf = FPDF(orientation="portrait" , unit="mm" , format="A4")
-
+    pdf_file_path = Path(file).stem
+    Pdf_file_name =  pdf_file_path.split("-")[0]
+    date = pdf_file_path.split("-")[1]
     pdf.add_page()
-    file_name = Path(file).stem
-    invoice_nm = file_name.split("-")[0]
-    date = file_name.split("-")[1]
-    pdf.set_font(family="times" , size=10 , style="B")
-    pdf.set_text_color(100 , 100 ,100)
-    pdf.cell(w= 0 , h= 10 , txt=f"Invoice Number: {invoice_nm}" , ln= 1)
-    pdf.cell(w=0 , h=10 , txt=f"Date: {date}")
-    pdf.output(f"invoices/pdf_file/{file_name}.pdf")
+    pdf.set_font(family="times" , size=14 , style="B")
+    pdf.set_text_color(0 , 0 , 0)
+    pdf.cell(w= 0 , h= 10 , txt=f"Invoice Number : {Pdf_file_name}" , align="L" , ln=1)
+    pdf.cell(w= 0 , h= 10 , txt=f"Date : {date}" , align="L" , ln=1)
+    pdf.output(f"invoices/pdf_file/{pdf_file_path}.pdf")
